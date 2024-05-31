19a9b528cea66a91f9facfe7220cb4c399798024
Trevor Rounce <trevor@switchbackcreative.ca>
2024-05-31 12:15:47 -0600

    added new poppins fonts

---
+++ src/data/siteData.js
@@ -0,0 +1,8 @@
+import defaultSrc from "../../public/default.jpg";
+
+export const baseData = {
+  title: "The Airdrie and District Community Foundation",
+  description: "The Airdrie and District Community Foundation",
+  siteImg: `https://airdrie-foundation.netlify.app/${defaultSrc.src}`,
+  url: "https://airdriefoundation.ca/",
+};
