bfaeefc211bd0f84fb5063acc8a012db4d7b348d
Trevor Rounce <trevor@switchbackcreative.ca>
2024-08-13 13:07:32 -0600

    added new mailchimp code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://airdriefoundation.ca/",
-  integrations: [react(), partytown()],
+  integrations: [react(), partytown(), sitemap()],
   image: {
     domains: ["airdriefoundation.swbdatabases3.com"],
   },
